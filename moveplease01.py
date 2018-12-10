83cf0fcd12f3f4246b81ad13432c11b279603fee
thilak.chandran <seethilakchandran@gmail.com>
2018-12-10 17:44:52 -0500

    lab 9

---
+++ moveplease01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import shutil
+import os
+
+os.chdir('/home/student/mycode/mycode')
+shutil.move('raynor.obj', 'ceph_storage/')
+xname = input('what is the new name for kerrigan.obj?')
+shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname)
